4125c4f3464139872676334f0205e393d40f5912
Stefan Lutschg <hirzli2@gmail.com>
2020-08-25 20:27:49 -0400

    Learning About Stacks

---
+++ stack.c
@@ -0,0 +1,74 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+struct Stack{
+    int top;
+    unsigned capacity;
+    int* array;
+
+};
+
+struct Stack* createStack(unsigned capacity){
+
+    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
+    stack->capacity = capacity;
+    stack->top = -1;
+    stack->array = (int*)malloc(stack->capacity * sizeof(int));
+    return stack;
+};
+
+int isFull(struct Stack* stack)
+{
+
+    return stack->top == stack->capacity - 1;
+
+
+}
+
+int isEmpty(struct Stack* stack){
+
+
+    return stack->top == -1;
+
+}
+
+
+void push(struct Stack* stack, int item) 
+{ 
+    if (isFull(stack)) 
+        return; 
+    stack->array[++stack->top] = item; 
+    printf("%d pushed to stack\n", item); 
+} 
+  
+// Function to remove an item from stack.  It decreases top by 1 
+int pop(struct Stack* stack) 
+{ 
+    if (isEmpty(stack)) 
+        return INT_MIN; 
+    return stack->array[stack->top--]; 
+} 
+  
+// Function to return the top from stack without removing it 
+int peek(struct Stack* stack) 
+{ 
+    if (isEmpty(stack)) 
+        return INT_MIN; 
+    return stack->array[stack->top]; 
+} 
+
+int main(){
+
+    struct Stack* stack = createStack(100);
+
+    push(stack, 10); 
+    push(stack, 20); 
+    push(stack, 30); 
+  
+    printf("%d popped from stack\n", pop(stack)); 
+  
+    return 0;
+}
